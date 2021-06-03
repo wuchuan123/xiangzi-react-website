ea003b360eb09219556e9b0c10a2adfa52cc9f48
吴川 <wuchuan@wuchuandeMacBook-Pro.local>
2021-06-03 08:08:07 +0800

    deploy

---
+++ precache-manifest.a6f1ab9d3f14a60210bd282f8aa1ff9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fa45f6a2961353065f195946009d92",
+    "url": "/xiangzi-react-website/index.html"
+  },
+  {
+    "revision": "cf45698e1a00356f50f9",
+    "url": "/xiangzi-react-website/static/css/main.53637434.chunk.css"
+  },
+  {
+    "revision": "312fa875fd5324880406",
+    "url": "/xiangzi-react-website/static/js/2.902035bc.chunk.js"
+  },
+  {
+    "revision": "de7c95d42723c2d4f76d99ad9a65a751",
+    "url": "/xiangzi-react-website/static/js/2.902035bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf45698e1a00356f50f9",
+    "url": "/xiangzi-react-website/static/js/main.73e96ac0.chunk.js"
+  },
+  {
+    "revision": "2dbc4edc78112128639e",
+    "url": "/xiangzi-react-website/static/js/runtime-main.a1e20807.js"
+  }
+]);
